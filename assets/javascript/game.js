b0f5535305a1489f57b38c7f79cd97e097ff7a4b
Deo Bissessar <Deo Bissessar>
2016-07-27 03:54:01 -0400

    tried updating js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,29 @@ $(document).ready(function() {
 		{
 			name: "Magneto",
 			img: "assets/images/magneto.jpg",
-			HP: "200",
-			ATK: "20",
-			CATK: "20"
+			HP: "100",
+			ATK: "14",
+			CATK: "5"
 		},
 		{
 			name: "Apocalypse",
 			img: "assets/images/apocalypse.jpg",
-			HP: "200",
-			ATK: "20",
-			CATK: "20"
+			HP: "180",
+			ATK: "7",
+			CATK: "25"
 		},
 		{
 			name: "Mr Sinister",
 			img: "assets/images/sinister.jpg",
-			HP: "200",
-			ATK: "20",
-			CATK: "20"
+			HP: "120",
+			ATK: "8",
+			CATK: "15"
 		},
 		{
 			name: "Holocaust",
 			img: "assets/images/holocaust.png",
-			HP: "200",
-			ATK: "20",
+			HP: "150",
+			ATK: "8",
 			CATK: "20"
 		}
 	];
@@ -36,7 +36,7 @@ $(document).ready(function() {
 
 	//add characters to html
 	var addplayers = function () {
-		$("#pool").html("<h3>Choose your player!</h3>");
+		$("#pool").html("<h3>Available Players</h3>");
 		$.each(characters, function (index, value) {
 			var $characterName = $("<p>").addClass("name text-center").html(characters[index].name);
 
@@ -48,7 +48,7 @@ $(document).ready(function() {
 
 			var $counter = $("<p>").addClass("counter text-center").html("Counter: " + characters[index].CATK);
 
-			var $characterDiv = $("<div>").addClass("character col-sm-3")
+			var $characterDiv = $("<div>").addClass("character col-sm-3 clearfix")
 								.append($characterPic, $characterName, $hitpoints, $attack, $counter)
 								.one("click", playerSelect);
 
@@ -58,18 +58,29 @@ $(document).ready(function() {
 
 	//player select
 	function playerSelect (event) {
-		$("#user").html("Player 1");
+		$("#user").html("<h3>Player 1</h3>");
 		$(this).appendTo("#user");
+		user = $(".character.name");
+		console.log("user " + user);
 		$("#pool").children().unbind("click", playerSelect);
 		$("#pool").children().one("click", enemySelect);
 	}
 
 	//enemy select
 	function enemySelect (event) {
-		$("#cpu").html("CPU")
+		$("#cpu").html("<h3>CPU</h3>");
 		$(this).appendTo("#cpu");
-		$("#pool").children().unbind("click", enemySelect);
+		var $battle = $("<button>").addClass("atkbtn btn btn-primary btn-lg").html("Attack!");
+		$("#cpu").append($battle);
+		$("#pool").children().unbind("click", enemySelect).hide();
 	}
+
+	//battle!!!
+	$(".atkbtn").on("click", function () {
+		if ($("#cpu"),children().length !==0){
+			var atkmsg = "You attacked for "
+		}
+	});
 
 	addplayers();
 	})
